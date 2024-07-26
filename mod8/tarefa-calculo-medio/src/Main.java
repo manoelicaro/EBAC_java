c7301dcf270c9d084dcd344b6117a8e873c97d48
manoelicaro <manoelicaro@gmail.com>
2024-07-26 16:29:29 -0400

    Projeto media creado

---
+++ mod8/tarefa-calculo-medio/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        double num1 = 10;
+        double num2 = 8;
+        double num3 = 6;
+        double num4 = 10;
+        double media = (num1 + num2 + num3 + num4) / 4;
+
+        System.out.println(media);
+    }
+}
