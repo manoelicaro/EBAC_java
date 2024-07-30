e1d9adeee1afe787782db165f062c57e8b306ae1
manoelicaro <manoelicaro@gmail.com>
2024-07-30 18:26:45 -0400

    Primeiro casting criado

---
+++ tarefa_wrappers/src/Exemplo.java
@@ -0,0 +1,25 @@
+// Primeiro exemplo de casting
+
+/**
+ * @author icarogaldino
+ */
+
+//Criar uma classe
+public class Exemplo {
+    public static void main(String[] args) {
+        //Criar variáveis
+        int x = 10;
+
+        //Imprimir a variável x
+        System.out.println(x);
+
+        //Criar variável
+        double y = 0;
+
+        //Converter a variável x para double
+        y = (double) x;
+
+        //Imprimir a variável z
+        System.out.println(y);
+    }
+}
